ad3ad51c9081461bc71b3d55768f9a5d54ffdcfd
edsobrinho <ednilson.sobrinho@gmail.com>
2017-08-01 01:29:40 -0300

    Ajustes na performace

--- assets/js/project/script.js
+++ assets/js/project/script.js
@@ -6,134 +6,21 @@
 // $(".pattern-overlay").css({ 'height': ($(window).height()) + 'px' });//Fix fullscreen pattern overlay height
 
 $(document).ready(function () {
-/*-----------------------------------------------------------------------------------*/
-/*	Sliders
-/*-----------------------------------------------------------------------------------*/
-    /* Home Fullscreen Slider */
-    $(".fullscreen-slider").backstretch([
-        "assets/img/bg/terapia-infantil.jpg",
-        "assets/img/bg/terapeuta-infantil.jpg",
-        "assets/img/bg/consultorio-psicologa-sao-paulo.jpg",
-        "assets/img/bg/consultorio-psicologa-infantil-em-sao-paulo.jpg"
-    ], { duration: 4500, fade: 650 });
-
-    /* Home Title Slider */
-    $('.title-slider').bxSlider({
-        mode: 'fade',
-        pager: false,
-        auto: true,
-        controls: false
-    });
-    /* Ipad Air Slider*/
-    $('#air-slider').bxSlider({
-        mode: 'fade',
-        pager: true,
-        auto: true,
-        controls: false
-    });
-    /* Blog Title Slider */
-    $('.blog-title-slider').bxSlider({
-        mode: 'fade',
-        pager: true,
-        auto: true,
-        controls: false
-    });
-
-    /* Portfolio Slider*/
-    $('#portfolio-slider').bxSlider({
-        mode: 'fade',
-        pager: true,
-        auto: true,
-        controls: false
-    });
-    /* Portfolio Featured Projects Slider*/
-    $('.portfolio-featured-projects-slider').bxSlider({
-        mode: 'fade',
-        pager: true,
-        auto: true,
-        controls: false
-    });
-    /*-----------------------------------------------------------------------------------*/
-    /*	Full Screen Video 
-    /*-----------------------------------------------------------------------------------*/
-   
-        $(".player").mb_YTPlayer();
-   
-
-    /*-----------------------------------------------------------------------------------*/
-    /*	Go Back  Top Button
-    /*-----------------------------------------------------------------------------------*/
-
-    
-        $("#go-back-top").hide();
-       
-            $(window).scroll(function () {
-                if ($(this).scrollTop() > 100) {
-                    $('#go-back-top').fadeIn();
-                } else {
-                    $('#go-back-top').fadeOut();
-                }
-            });
-      
-
-    
-
-    /*-----------------------------------------------------------------------------------*/
-    /*	Date Picker
-    /*-----------------------------------------------------------------------------------*/
-    
-        // $("#datepicker").datepicker();
-
-    /*-----------------------------------------------------------------------------------*/
-    /*	Pricing Table
-    /*-----------------------------------------------------------------------------------*/
-    $('body').on('mouseenter', '#pricing-table-3-columns .plan', function () {
-        $('.plan').removeClass('plan-active');
-        $(this).addClass('plan-active');
-    });
-    $('body').on('mouseleave', '#pricing-table-3-columns .plan', function () {
-        $('.plan').removeClass('plan-active');
-        $('#most-popular').addClass('plan-active');
-    })
-    $('body').on('mouseenter', '#pricing-table-4-columns .plan', function () {
-        $('.plan').removeClass('plan-active');
-        $(this).addClass('plan-active');
-    });
-    $('body').on('mouseleave', '#pricing-table-4-columns .plan', function () {
-        $('.plan').removeClass('plan-active');
-        $('#most-popular').addClass('plan-active');
-    })
-    /*-----------------------------------------------------------------------------------*/
-    /*	Progress Bars
-    /*-----------------------------------------------------------------------------------*/
-
-   
-        //$('.progress .progress-bar').progressbar({ display_text: 'fill' });
  
-    /*-----------------------------------------------------------------------------------*/
-    /*	Our Numbers
-    /*-----------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------*/
+/*	Go Back  Top Button
+/*-----------------------------------------------------------------------------------*/
 
-    
-        var shownx = false;
 
-        $('#our-numbers').waypoint(function () {
-
-            if (!shownx) {
-
-                function count(options) {
-                    var $this = $(this);
-                    options = $.extend({}, options || {}, $this.data('countToOptions') || {});
-                    $this.countTo(options);
-                    shownx = true;
-                }
-                $('.timer').each(count);
-            }
-
-        }, { offset: '80%' });
+    $("#go-back-top").hide();
    
-
-
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#go-back-top').fadeIn();
+        } else {
+            $('#go-back-top').fadeOut();
+        }
+    });
 });
 
 
@@ -141,88 +28,8 @@ window.onload = function () {
 /*-----------------------------------------------------------------------------------*/
 /*	PreLoader
 /*-----------------------------------------------------------------------------------*/
-    
-        $('#status').fadeOut();
-        $('#preloader').delay(350).fadeOut('slow');
-        $('body').delay(350).css({ 'overflow': 'visible' });
+    $('#status').fadeOut();
+    $('#preloader').delay(350).fadeOut('slow');
+    $('body').delay(350).css({ 'overflow': 'visible' });
  /*End PreLoader*/
-/*-----------------------------------------------------------------------------------*/
-/* Google Map Styles
-/*-----------------------------------------------------------------------------------*/
-    'use strict';
-
-    var latlng = new google.maps.LatLng(-23.5732270, -46.639267);
-
-    var styles = [
-
-                 {
-                     "featureType": "road",
-                     "stylers": [
-                       { "color": "#B382BA" }
-                     ]
-                 }, {
-                     "featureType": "landscape",
-                     "stylers": [
-                       { "color": "efefef" }
-                     ]
-                 }, {
-                     "elementType": "labels.text.fill",
-                     "stylers": [
-                       { "color": "#444751" }
-                     ]
-                 }, {
-                     "featureType": "poi",
-                     "stylers": [
-                       { "color": "efefef" }
-                     ]
-                 }, {
-                     "elementType": "labels.text",
-                     "stylers": [
-
-                       { "weight": 0.1 },
-                       { "color": "#444751" }
-                     ]
-                 }, {
-                 }
-
-    ];
-
-
-    var myOptions = {
-        zoom: 16,
-        center: latlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        disableDefaultUI: true,
-        scrollwheel: false,
-        styles: styles
-    };
-
-    var markerNotice = '<div id="marker-content">' +
-     '<div id="marker-notice">' +
-     '</div>' +
-     '<h4>Terapia dos Pequenos</h4>' +
-     '<p>Psicologia Infantil' +
-     '</p>' +
-     '</div>';
-
-    var infowindow = new google.maps.InfoWindow({
-        content: markerNotice
-    });
-
-    var map = new google.maps.Map(document.getElementById('map'), myOptions);
-
-    var markerPosition = new google.maps.LatLng(-23.574107, -46.639267);
-
-    var markerImage = 'assets/img/other/marker.png';
-    var marker = new google.maps.Marker({
-        position: markerPosition,
-        map: map,
-        title: 'Inc.!',
-        icon: markerImage
-    });
-
-    google.maps.event.addListener(marker, 'click', function () {
-        infowindow.open(map, marker);
-    });
-
 }
--- assets/js/project/text-rotator.js
+++ assets/js/project/text-rotator.js
@@ -1,19 +1,4 @@
-﻿$("#rotate-fade-in").Morphext({
-    animation: "fadeIn",
-    separator: ",",
-    speed: 3000,
-});
-$("#rotate-bounce-in").Morphext({
-    animation: "bounceIn",
-    separator: ",",
-    speed: 3000,
-});
-$("#rotate-shake").Morphext({
-    animation: "shake",
-    separator: ",",
-    speed: 3000,
-});
-$("#rotate-tada").Morphext({
+﻿$("#rotate-tada").Morphext({
     animation: "fadeInLeft",
     separator: ",",
     speed: 5000,
@@ -47,14 +32,4 @@ $("#rotate-tada-6").Morphext({
     animation: "fadeInLeft",
     separator: ",",
     speed: 5000,
-});
-$("#rotate-wobble").Morphext({
-    animation: "wobble",
-    separator: ",",
-    speed: 3000,
-});
-$("#rotate-jello").Morphext({
-    animation: "jello",
-    separator: ",",
-    speed: 3000,
 });
