a5e12106f82eee5a5def4e49fa2794cd7b768bd7
edsobrinho <ednilson.sobrinho@gmail.com>
2017-08-02 02:09:13 -0300

    Ajustes site

--- assets/js/project/text-rotator.js
+++ assets/js/project/text-rotator.js
@@ -1,25 +1,5 @@
-<<<<<<< HEAD
-﻿$("#rotate-fade-in").Morphext({
-    animation: "fadeIn",
-    separator: ",",
-    speed: 3000,
-});
-$("#rotate-bounce-in").Morphext({
-    animation: "bounceIn",
-    separator: ",",
-    speed: 3000,
-});
-$("#rotate-shake").Morphext({
-    animation: "shake",
-    separator: ",",
-    speed: 3000,
-});
 $("#rotate-tada").Morphext({
     animation: "fadeIn",
-=======
-﻿$("#rotate-tada").Morphext({
-    animation: "fadeInLeft",
->>>>>>> ad3ad51c9081461bc71b3d55768f9a5d54ffdcfd
     separator: ",",
     speed: 8000,
 });
@@ -51,19 +31,5 @@ $("#rotate-tada-5").Morphext({
 $("#rotate-tada-6").Morphext({
     animation: "fadeIn",
     separator: ",",
-<<<<<<< HEAD
     speed: 8000,
-});
-$("#rotate-wobble").Morphext({
-    animation: "wobble",
-    separator: ",",
-    speed: 3000,
-});
-$("#rotate-jello").Morphext({
-    animation: "jello",
-    separator: ",",
-    speed: 3000,
-=======
-    speed: 5000,
->>>>>>> ad3ad51c9081461bc71b3d55768f9a5d54ffdcfd
 });
